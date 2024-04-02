258c3eaea7ccab661c09910a82ab81d1beaf72f4
rebel <allesrebel@yahoo.com>
2024-04-01 22:39:29 -0700

    updated script + added jetson containers submodule

--- run_this_on_native.sh
+++ run_this_on_native.sh
@@ -1,3 +1,28 @@
-#! /bin/bash
+#!/bin/bash
 
-~/nvme/jetson-containers/run.sh --volume $PWD:/root $(~/nvme/jetson-containers/autotag ros:humble-desktop opencv:cuda l4t-ml )
+echo "Checking for Jetson Containers Environment Var"
+JETSON_CONTAINERS=${JETSON_CONTAINERS:-"$PWD/jetson-containers"}
+echo "Jetson Containers Dir: $JETSON_CONTAINERS"
+
+echo "Running autotag to get image tags..."
+AUTOTAG_COMMAND="$($JETSON_CONTAINERS/autotag ros:humble-desktop opencv:cuda l4t-ml)"
+echo "Autotag Command Output: $AUTOTAG_COMMAND"
+
+# Check if the number of arguments is greater than 0
+if [ $# -gt 0 ]; then
+    echo "Passing Argument to Docker: $@"
+    echo "$JETSON_CONTAINERS/run.sh --volume $PWD:/root --workdir /root $AUTOTAG_COMMAND run_this_in_docker.sh"
+    $JETSON_CONTAINERS/run.sh \
+        --volume $PWD:/root \
+        --workdir /root \
+        $AUTOTAG_COMMAND \
+        "$@"
+else
+    echo "Defaulting to Build OrbSlam 3 only."
+    echo "$JETSON_CONTAINERS/run.sh --volume $PWD:/root --workdir /root $AUTOTAG_COMMAND run_this_in_docker.sh"
+    $JETSON_CONTAINERS/run.sh \
+        --volume $PWD:/root \
+        --workdir /root \
+        $AUTOTAG_COMMAND \
+        "/root/run_this_in_docker.sh"
+fi
