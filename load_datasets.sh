2eb069cd421bdcb957ddaed03203f1b098978b59
rebel <allesrebel@yahoo.com>
2024-04-14 20:20:33 -0700

    Added dataset download script

---
+++ load_datasets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p Datasets && cd Datasets && mkdir -p EuRoc
+
+# MH_01
+mkdir -p MH01 && cd MH01
+wget http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/machine_hall/MH_01_easy/MH_01_easy.zip
+unzip MH_01_easy.zip
+cd ..
+
+# MH_02
+mkdir -p MH02 && cd MH02
+wget http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/machine_hall/MH_02_easy/MH_02_easy.zip
+unzip MH_02_easy.zip
+cd ..
