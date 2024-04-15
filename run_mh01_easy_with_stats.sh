80dbc23c3a47ef878bd522117365ecea9718e807
rebel <allesrebel@yahoo.com>
2024-04-14 20:51:27 -0700

    Collects results

--- run_mh01_easy_with_stats.sh
+++ run_mh01_easy_with_stats.sh
@@ -17,3 +17,7 @@ cd orb_slam3
 # finally do the actual orbslam with sample video
 ./Examples/Stereo/stereo_euroc ./Vocabulary/ORBvoc.txt ./Examples/Stereo/EuRoC.yaml ~/Datasets/EuRoc/MH01 ./Examples/Stereo/EuRoC_TimeStamps/MH01.txt dataset-MH01_stereo
 
+# save results into results folder
+cd && mkdir -p result_mh01
+mv perf.data jtop_stats.log result_mh01/
+mv orb_slam3/ExecMean.txt orb_slam3/LocalMapTimeStats.txt orb_slam3/ExecMean.txt orb_slam3/f_dataset-MH01_stereo.txt orb_slam3/SessionInfo.txt orb_slam3/kf_dataset-MH01_stereo.txt orb_slam3/LBA_Stats.txt orb_slam3/TrackingTimeStats.txt result_mh01/
