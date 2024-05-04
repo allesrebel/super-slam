0624e54fcfeb22a1c01bac29ba7fa01a0e979118
rebel <allesrebel@yahoo.com>
2024-05-04 15:42:18 -0700

    Removed unused stuff for regular builds

--- stats/collect_jtop_stats.py
+++
@@ -1,7 +0,0 @@
-from jtop import jtop
-
-with jtop() as jetson:
-    # jetson.ok() will provide the proper update frequency
-    while jetson.ok():
-        # Read tegra stats
-        print(jetson.stats)
