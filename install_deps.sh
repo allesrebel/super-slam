2fb79873071f9a5c98543e8c3de071f936eaf73a
Thong Le <lebathong188@gmail.com>
2024-05-05 17:44:42 -0700

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt install libopencv-dev python3-opencv libboost-serialization-dev cmake
+apt install -y libeigen3-dev cmake g++ wget unzip libopencv-dev python3-opencv libboost-all-dev libssl-dev
